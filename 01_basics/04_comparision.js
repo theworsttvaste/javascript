cc938c8e59740444175d5103121cb43d946a019b
wallflower <164307210+theworsttvaste@users.noreply.github.com>
2025-07-16 17:05:44 +0000

    data_comaprisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+
+
+
+/*************************AVOID USING THESE************************/
+
+
+//  console.log("02" > 1);
+//  console.log("2" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+// ===
+console.log("2" === 0);
+
