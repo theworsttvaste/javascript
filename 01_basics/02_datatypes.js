ef0acfbeff34a3d721b5e6799116a7d5a4949b61
wallflower <164307210+theworsttvaste@users.noreply.github.com>
2025-07-16 14:10:33 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all the js code as newer version/code
+
+//alert("hello") // we are using nodejs, not bowser, so it wont work
+
+let name = "beauty!"
+let age = 21
+let isloggedIn = true
+
+
+//premitive datatypes
+
+// number => 2^53
+//bigint = use if number is very big
+//string = ""
+// boolean  = true/false
+// null = standalone value
+//undefined = value hasnt been defined
+//symbol = unique
+
+
+
+console.log(typeof age);//to check the type of data
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
