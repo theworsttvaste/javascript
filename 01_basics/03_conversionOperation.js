241d2b337a22dd7eebf3c4a62f8a8086bb15ef2b
wallflower <164307210+theworsttvaste@users.noreply.github.com>
2025-07-16 15:12:24 +0000

    conversion and operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,65 @@
+ //let score = "33" // output = string
+ // let score = null // output  = object
+ //let score = undefined // undefined
+ //let score = true // output = 1
+
+//let valueInNumber = Number (score)
+
+
+
+/*console.log(typeof score);
+console.log(score);
+console.log(valueInNumber);*/
+
+// "33" = 33 
+// "33abc" = NaN // not in number but type is number
+// true = 1; false = 0
+
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 = true; 0 = false
+// " " = false // empty string = false
+// "beauty"  = true
+
+// let number = 21
+// let stringNumber = String(number)
+// console.log(stringNumber);
+// console.log( typeof stringNumber);
+
+/************************ OPERATIONS ********************/
+ 
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); 2 raise to power 2
+// console.log(2/2);
+// console.log(2%2); // gives remainder
+
+// let str1 = "hello" 
+// let str2 = " beauty!"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + 2); output = 3
+// console.log("1" +2); output = 12
+// console.log(1 + "2"); output = 12
+// console.log("1" + "2"); output = 12
+// console.log(1 + 2 + "2"); output = 32
+
+
+
+
+
+
+
+
+
+
